5e2a2ae7c361b80ca4c7705f8414b808ae644b02
YulingGu95 <gu.349@osu.edu>
2017-03-10 14:46:17 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,8 @@
+setwd("C:/Users/betty/Desktop/coursera/exploratory data analysis/week-1")
+ HousePowerConsumption <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+ usedata <- HousePowerConsumption[HousePowerConsumption$Date %in% c("1/2/2007","2/2/2007") ,]
+ globalactivepower <- as.numeric(usedata$Global_active_power)
+ png("plot1.png", width=480, height=480)
+ hist(globalactivepower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+ dev.off()
+ 
